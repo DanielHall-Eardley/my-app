594da6916b895199d53f1e87cdd2cb0ead90a757
DanielHall-Eardley <350chevy8@gmail.com>
2022-01-23 20:03:02 -0500

    Progress sync

--- util/createHTMLComponent.js
+++ util/createHTMLComponent.js
@@ -4,7 +4,7 @@
     {
       name: "html element",
       attributes: [
-        { att: "attribute name", value: "attribute value"}
+        { name: "attribute name", value: "attribute value"}
       ],
       content: "inner text"
       children: [
@@ -14,6 +14,42 @@
   ]
 */
 
-exports.createHTMLContent = function (compStructure) {
-  const parent = new DocumentFragment()
+function addAttributesToElement (htmlELement, attributes) {
+  const newHtmlElement = htmlELement;
+
+  if (attributes && attributes.length > 0) {
+    attributes.forEach(obj => {
+      const { name, value} = obj;
+      newHtmlElement.setAttribute(name, value);
+    })
+  }
+
+  return newHtmlElement;
+}
+
+function addTextToElement (htmlELement, text) {
+  const escapedText = document.createTextNode(text);
+  htmlELement.innerText = escapedText
+  return htmlELement;
+}
+
+function recurseStructure (structure) {
+
+}
+
+exports.createHTMLComponent = function (compStructure) {
+  const component = new DocumentFragment();
+
+  compStructure.forEach(obj => {
+    const { name, attributes, content, children } = obj;
+    const htmlElement = document.createElement(name);
+    const htmlElementAtt = addAttributesToElement(htmlElement, attributes);
+    const htmlElementText = addTextToElement(htmlElementAtt, content);
+    component.append(htmlElementText);
+
+    if (children && children.length > 0) {
+      createHTMLComponent(children)
+    }
+  })
+
 }
