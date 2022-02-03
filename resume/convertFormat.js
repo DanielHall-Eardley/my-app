106315fbc060f76bdc9bd2a275973e9fd2680df6
DanielHall-Eardley <350chevy8@gmail.com>
2022-02-03 13:23:10 -0500

    progress sync

--- resume/convertFormat.js
+++ resume/convertFormat.js
@@ -8,5 +8,5 @@ const filename = `./Daniel Hall-Eardley ${timestamp}.pdf`
 
 pdf.create(html, options).toFile(filename, function(err, res) {
   if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
+  console.log(res); 
 });
