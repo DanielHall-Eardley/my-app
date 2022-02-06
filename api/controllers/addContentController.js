b6aabe61fa3fd08d32eede81929eefb6a972e90e
DanielHall-Eardley <350chevy8@gmail.com>
2022-02-05 20:22:49 -0500

    progress sync

--- api/controllers/addContentController.js
+++ api/controllers/addContentController.js
@@ -1,21 +1,25 @@
 const { initDB } = require('../db/db');
 const db = initDB();
 const catchAsyncError = require('../../util/catchAsyncError');
+const { uuid: v4 } = require('uuid');
 
-function createParams (formBody, customFields) {
+/* Create param object from the request body, 
+plus any custom fields */
+function createParams (formBody, customFields={}) {
   const fieldNames = Object.keys(formBody)
-  console.log(fieldNames)
-  /* formdata needs to always match the db model */
-  const formData = fieldNames.reduce((name, obj) => {
-    const paramName = `$${name}`;
-    obj[paramName] = formBody[name];
-    return obj;
-  }, {});
 
+  /* formdata needs to always match the db model */
+  const formData = {}
+  for(let name of fieldNames) {
+    const paramName = `$${name}`;
+    formData[paramName] = formBody[name];
+  }
+ 
   const paramObj = { ...formData, ...customFields };
   return paramObj;
 }
 
+/* Create the sql query dynamically */
 function createInsertQuery (paramObj, tableName) {
   const paramNames = Object.keys(paramObj);
   const columnNames = paramNames.map(name => name.slice(1));
@@ -25,7 +29,7 @@ function createInsertQuery (paramObj, tableName) {
   ) VALUES (
     ${paramNames.toString()}
   );
-`
+` 
 
   return query;
 }
@@ -34,29 +38,44 @@ exports.postBlog = catchAsyncError(async (req, res, next) => {
   const customParams = {
     $url: req.file.path,
   }
+
   const params = createParams(req.body, customParams);
   const query = createInsertQuery(params, 'blog');
-
   db.run(query, params);
   // redirect to blog page
 })
 
 exports.postAhaMoment = catchAsyncError(async (req, res, next) => {
-  console.log(req.body)
-  
+  const params = createParams(req.body);
+  const query = createInsertQuery(params, 'breakthru');
+  db.run(query, params);
 })
 
 exports.postDadHack = catchAsyncError(async (req, res, next) => {
-  console.log(req.body)
-  
+  const params = createParams(req.body);
+  const query = createInsertQuery(params, 'dadhack');
+  db.run(query, params);
 })
 
 exports.postMainProject = catchAsyncError(async (req, res, next) => {
-  console.log(req.body)
+  const customEmployerParams = {
+    employer_id: uuid(),
+    name: req.body.employerName,
+    addres
+  }
+
+  const employerParams = createParams(req.body);
+  const employerQuery = createInsertQuery(employerParams, 'employer');
+  db.run(employerQuery, params);
+
+  const params = createParams(req.body);
+  const query = createInsertQuery(params, 'main_project');
+  db.run(query, params);
   
 })
 
 exports.postSideProject = catchAsyncError(async (req, res, next) => {
-  console.log(req.body)
-  
+  const params = createParams(req.body);
+  const query = createInsertQuery(params, 'side_project');
+  db.run(query, params);
 })
--- api/models/dadHack.js
+++ api/models/dadHack.js
@@ -1,5 +1,5 @@
 const dadHacks = `
-  CREATE TABLE IF NOT EXISTS main_project (
+  CREATE TABLE IF NOT EXISTS dadhack (
     title VARCHAR(50) NOT NULL,
     category VARCHAR(50),
     date DATE NOT NULL,
